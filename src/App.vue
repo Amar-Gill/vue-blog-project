4b204d16b87e387becb759fec9678fc2d43a66cd
Amardeep Gill <amardeepgill@BML7HClearadmins-MacBook-Pro.local>
2020-12-21 17:06:05 -0500

    initialize router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <showBlogs/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-// import addBlog from './components/addBlog.vue';
-import showBlogs from './components/showBlogs.vue';
 
 export default {
   name: 'App',
-  components: {
-    // addBlog,
-    showBlogs
-  }
 }
 </script>
 
