5d697c3f21ff1f9fa3a8207274cbc0886dafdd56
Amardeep Gill <amardeepgill@BML7HClearadmins-MacBook-Pro.local>
2020-12-21 15:36:48 -0500

    added custom directives

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,11 @@
 <template>
-<div id="show-blogs">
+<div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <template v-for="blog in blogs">
-    <div :key="blog.id" class="single-blog">{{blog.title}}</div>
+    <div :key="blog.id" class="single-blog">
+        <h2 v-rainbow>{{blog.title}}</h2>
+        <article>{{blog.body}}</article>
+    </div>
     </template>
   
 </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Custom directives
+Vue.directive('rainbow', {
+  bind(el, ) {
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding) {
+    if (binding.value === 'wide') {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value === 'narrow') {
+      el.style.maxWidth = "650px"
+    }
+    if (binding.arg == 'column') {
+      el.style.background = "#ddd";
+      el.style.padding = "20px";
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
