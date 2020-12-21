c8dd920903bad41eac933a46d99134d6ae44316f
Amardeep Gill <amardeepgill@BML7HClearadmins-MacBook-Pro.local>
2020-12-21 16:46:37 -0500

    locally registered filters and directives

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -23,9 +23,31 @@ export default {
   },
   computed: {
       filteredBlogs() {
-          return this.blogs.filter(blog => {
-             return blog.title.match(this.search);
-          });
+          return this.blogs.filter(blog => blog.title.match(this.search))
+      }
+  },
+  filters: {
+      toUppercase(value) {return value.toUpperCase()}, // allow for use of 'to-uppercase' in markup
+      snippet(value) {return value.slice(0,100) + '...'}
+  },
+  directives: {
+      'rainbow': {
+          bind(el, ) {
+             el.style.color = "#" + Math.random().toString().slice(2,8);
+          }
+      },
+      'theme': {
+           bind(el, binding) {
+            if (binding.value === 'wide') {
+            el.style.maxWidth = "1200px";
+            } else if (binding.value === 'narrow') {
+            el.style.maxWidth = "650px"
+            }
+            if (binding.arg == 'column') {
+            el.style.background = "#ddd";
+            el.style.padding = "20px";
+            }
+        }
       }
   },
   methods: {
--- src/main.js
+++ src/main.js
@@ -3,36 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// Custom directives
-Vue.directive('rainbow', {
-  bind(el, ) {
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-});
-
-Vue.directive('theme', {
-  bind(el, binding) {
-    if (binding.value === 'wide') {
-      el.style.maxWidth = "1200px";
-    } else if (binding.value === 'narrow') {
-      el.style.maxWidth = "650px"
-    }
-    if (binding.arg == 'column') {
-      el.style.background = "#ddd";
-      el.style.padding = "20px";
-    }
-  }
-});
-
-// Filters
-Vue.filter('to-uppercase', function(value) {
-  return value.toUpperCase();
-})
-
-Vue.filter('snippet', function(value) {
-  return value.slice(0,100) + '...';
-})
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
